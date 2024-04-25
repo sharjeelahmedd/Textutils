4656f00503447ea8b3518f91b6070c22fc053925
sharjeelahmedd <168014182+sharjeelahmedd@users.noreply.github.com>
2024-04-25 22:44:38 +0500

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import dza, {b, c, d} from './module2.mjs'
+console.log(dza);
+console.log(d);
+console.log(c);
+console.log(b);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Sharjeel";
+const b = "Ali";
+const c = "M. Ali";
+const d = "Naba";
+
+export default a;
+export {b};
+export {c};
+export {d};
